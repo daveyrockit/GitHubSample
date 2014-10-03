7116f4e5a35a805156c7c87d248e63dbe97c759f
daveyrockit <daveyrockit14@gmail.com>
2014-10-02 15:08:56 -1000

    Our initial commit
    created Xcode project

---
+++ GitHubProject/GitHubProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitHubProject
+//
+//  Created by Dave Doles on 10/2/14.
+//  Copyright (c) 2014 ___davedoles___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
